edf5c791a08c62b937278d7fa603f87b1cd4f9ff
Martin Liberg <martin@steep.app>
2023-10-22 20:46:26 +0200

    Add text and images

--- src/Notes.tsx
+++ src/Notes.tsx
@@ -11,48 +11,51 @@ export default function Notes() {
     <>
       {notes.map((note) => (
         <Fragment key={note.id}>
-          <div id={"note-" + note.id}>
-            {note.text && (
-              <div
-                contentEditable
-                dangerouslySetInnerHTML={{ __html: note.text }}
-                suppressContentEditableWarning
-                onBlur={(event) => {
-                  if (!note.id) return
-                  const text = event.currentTarget.innerHTML.trim()
-                  console.log(text)
-                  if (text === "<br>") db.notes.delete(note.id)
-                  else db.notes.update(note.id, { text })
-                }}
-              />
-            )}
-            {note.imageUrl && (
-              <img src={note.imageUrl} style={{ maxWidth: "100%" }} />
-            )}
-          </div>
-          <hr />
+          {note.text && (
+            <div
+              className="p-2 border-b outline-none"
+              contentEditable
+              dangerouslySetInnerHTML={{ __html: note.text }}
+              suppressContentEditableWarning
+              onBlur={(event) => {
+                if (!note.id) return
+                const text = event.currentTarget.innerHTML.trim()
+                if (text === "<br>") db.notes.delete(note.id)
+                else db.notes.update(note.id, { text })
+              }}
+            />
+          )}
+          {note.imageUrl && <img src={note.imageUrl} />}
         </Fragment>
       ))}
       <div
+        className="p-2 border-b outline-none"
         contentEditable
         suppressContentEditableWarning
+        onFocus={(event) => (event.currentTarget.innerHTML = "")}
         onBlur={(event) => {
-          if (event.currentTarget.innerHTML === PLACEHOLDER_TEXT) return
-          createTextNote(event.currentTarget.innerHTML)
+          const text = event.currentTarget.innerHTML.trim()
+          if (text === PLACEHOLDER_TEXT || text === "<br>") return
+          db.notes.add({ text })
           event.currentTarget.innerHTML = PLACEHOLDER_TEXT
         }}
       >
         {PLACEHOLDER_TEXT}
       </div>
-      <input
-        type="file"
-        onChange={async (event) => {
-          const [file] = event.target.files ?? []
-          if (!file) return
-          event.currentTarget.value = ""
-          db.notes.add({ image: await file.arrayBuffer() })
-        }}
-      />
+      <label htmlFor="new-image" className="block p-2 outline-none">
+        New image
+        <input
+          id="new-image"
+          className="hidden"
+          type="file"
+          onChange={async (event) => {
+            const [file] = event.target.files ?? []
+            if (!file) return
+            event.currentTarget.value = ""
+            db.notes.add({ image: await file.arrayBuffer() })
+          }}
+        />
+      </label>
     </>
   )
 }
@@ -69,8 +72,4 @@ function useNotes() {
       },
     })
   )
-}
-
-function createTextNote(text: string) {
-  db.notes.add({ text })
 }
